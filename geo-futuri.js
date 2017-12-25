cdc5cd65a308b92c9e92da496463a6085ea0e833
sifbuilder <sifbuilder@gmail.com>
2017-12-25 01:01:58 +0100

    524 dynafuturi g.futuri

--- geo-futuri.js
+++ geo-futuri.js
@@ -1,12 +1,12 @@
 /*******************************************
- * 			@geofuturi
- * 
- */	
+ *      @geofuturi
+ *
+ */
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.geofuturi = global.geofuturi || {})));
-}(this, function (exports) { 'use strict';
+  typeof exports === "object" && typeof module !== "undefined" ? factory(exports) :
+    typeof define === "function" && define.amd ? define(["exports"], factory) :
+      (factory((global.geofuturi = global.geofuturi || {})))
+}(this, function (exports) { "use strict"
 
 // Philippe Rivière’s Block 14ddff5e46b6fe9341dae91c3c83304b
 // Updated August 6, 2017
@@ -18,101 +18,95 @@
 // forked from Fil's block: Furuti 3 - projection.clipPolygon()
 // LICENSE# Released under the The MIT License.
 
-var geofuturi = function geofuturi(__mapper = {}) {
+  let geofuturi = function geofuturi(__mapper = {}) {
 
-	let renderer = __mapper("renderRenderer"),
-			width = renderer.width(),
-			height = renderer.height()
-	
-	let f = __mapper({"props": bosonProps.bosonProps()}).props()
-	let g = __mapper("xs").m("geom")
-	let w = __mapper("xs").m("wen")
-	let versor = __mapper("xs").b("versor")()
-	let wen = __mapper("xs").c("wen")()
+    let renderer = __mapper("renderRenderer"),
+      width = renderer.width(),
+      height = renderer.height(),
+      scaleProj = Math.min(width/2, height)/Math.PI
 
-	let scaleProj = Math.min(width/2, height)/Math.PI
-	let epsilon = 1e-6, epsilon2 = epsilon * epsilon
-	let asin = Math.asin, atan = Math.atan
-	let pi = Math.PI, degrees = 180 / pi, radians = pi / 180 
-	let asin1_3 = Math.asin(1 / 3)
-	let sqrt1_2 = Math.SQRT1_2
-	let abs = Math.abs;
-  let eps = 1e-6	
-	
+    let f = __mapper({"props": bosonProps.bosonProps()}).props()
+    let g = __mapper("xs").m("geom")
 
-/*******************************************
- * 			@polyhedron
- */	
-let vertices = []
-	let mode = "cartesian"
-	if ( mode === "spherical") {
-		let ang = 90,
-				phi1 = atan(sqrt1_2) * degrees		// 35.264389682754654
-				
-		vertices = [
-			[ (-1 * ang - 45),  phi1], // 3
-			[ (0 *  ang - 45),  phi1], // 0
-			[ (1 *  ang - 45),  phi1], // 1
-			[ (2 *  ang - 45),  phi1], // 2
-			[ (-1 * ang - 45), -phi1], // 7
-			[ (0 *  ang - 45), -phi1], // 4
-			[ (1 *  ang - 45), -phi1], // 5
-			[ (2 *  ang - 45), -phi1], // 6
-		]	
- } else {
+    let atan = Math.atan
+    let pi = Math.PI, degrees = 180 / pi
+    let sqrt1_2 = Math.SQRT1_2
 
-		vertices = [
-			[-1, -1,  1], // 0	// 0
-			[ 1, -1,  1], // 1	// 1
-			[ 1,  1,  1], // 2	// 2
-			[-1,  1,  1], // 3	// 3
-			[-1, -1, -1], // 5	// 4
-			[ 1, -1, -1], // 4	// 5
-			[ 1,  1, -1], // 7	// 6
-			[-1,  1, -1], // 6	// 7
-		].map(g.normalize)
-				.map(g.spherical)
-					.map(g.to_degrees)
+    /*******************************************
+ *      @polyhedron
+ */
+    let vertices = []
+    let mode = "cartesian"
+    if ( mode === "spherical") {
+      let ang = 90,
+        phi1 = atan(sqrt1_2) * degrees    // 35.264389682754654
 
-		vertices = d3.merge([
-			vertices,
-		])
-		
-	}
-	
-let faces = [
-			[1, 0, 3, 2, 1], // N
-			[1, 2, 6, 5, 1],
-			[2, 3, 7, 6, 2],
-			[3, 0, 4, 7, 3],
-			[0, 1, 5, 4, 0],
-			[5, 6, 7, 4, 5] // S
-		].map(function(face) {
-				return face.map(function(i) {
-					return vertices[i]
-  })
-})
+      vertices = [
+        [ (-1 * ang - 45),  phi1], // 3
+        [ (0 *  ang - 45),  phi1], // 0
+        [ (1 *  ang - 45),  phi1], // 1
+        [ (2 *  ang - 45),  phi1], // 2
+        [ (-1 * ang - 45), -phi1], // 7
+        [ (0 *  ang - 45), -phi1], // 4
+        [ (1 *  ang - 45), -phi1], // 5
+        [ (2 *  ang - 45), -phi1], // 6
+      ]
+    } else {
+
+      vertices = [
+        [-1, -1,  1], // 0  // 0
+        [ 1, -1,  1], // 1  // 1
+        [ 1,  1,  1], // 2  // 2
+        [-1,  1,  1], // 3  // 3
+        [-1, -1, -1], // 5  // 4
+        [ 1, -1, -1], // 4  // 5
+        [ 1,  1, -1], // 7  // 6
+        [-1,  1, -1], // 6  // 7
+      ].map(g.normalize)
+        .map(g.spherical)
+        .map(g.to_degrees)
+
+      vertices = d3.merge([
+        vertices,
+      ])
+
+    }
+
+    let faces = [
+      [1, 0, 3, 2, 1], // N
+      [1, 2, 6, 5, 1],
+      [2, 3, 7, 6, 2],
+      [3, 0, 4, 7, 3],
+      [0, 1, 5, 4, 0],
+      [5, 6, 7, 4, 5] // S
+    ].map(function(face) {
+      return face.map(function(i) {
+        return vertices[i]
+      })
+    })
 
 
-/*******************************************
- * 			@enty
- */	
-let enty = function( p={} ) {	
-	if (!p.faciaRotation) p.faciaRotation = Math.PI / 4
-	if (!p.geoRotation) p.geoRotation = c => [-c[0], -c[1], 0]
-	if (!p.tree) p.tree = [-1, 4, 5, 2, 0, 1] // [-1, 0, 0, 0, 0, 4] // 
-	if (!p.rotate) p.rotate = [28,-4,0] // California
-	if (!p.prjRaw) p.prjRaw = d3.geoGnomonicRaw
-	
-	if (!p.faces) p.faces = faces
+    /*******************************************
+ *      @enty
+ */
+    let enty = function( p={} ) {
 
-	return __mapper("xs").b("polyhedral")(p)
+      if (!p.faciaRotation) p.faciaRotation = Math.PI / 4
+      if (!p.geoRotation) p.geoRotation = c => [-c[0], -c[1], 0]
+      if (!p.tree) p.tree = [-1, 4, 5, 2, 0, 1]
+      if (!p.rotate) p.rotate = [28,-4,0] // California
 
-}
+      if (!p.faces) p.faces = faces
 
-	return enty
-}
+      if (!p.prjRaw) p.prjRaw = d3.geoGnomonicRaw
 
-exports.geofuturi = geofuturi
+      return __mapper("xs").b("polyhedral")(p)
 
-}));
+    }
+
+    return enty
+  }
+
+  exports.geofuturi = geofuturi
+
+}))
