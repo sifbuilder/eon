9f932182740950a5839908990529276b14b472d1
sifbuilder <sifbuilder@gmail.com>
2018-02-04 20:23:51 +0100

    533 cwen

--- control-wen.js
+++ control-wen.js
@@ -20,16 +20,16 @@
     let drag = d3.drag()
 
     function momentum () {
-      if (Math.abs(state.vel[0]) < 0.001 && Math.abs(state.vel[1]) < 0.001) return
+      if (Math.abs(state.vel[0]) < inits.epsilon && Math.abs(state.vel[1]) < inits.epsilon) return
       state.vel[0] *= inits.decay; state.vel[1] *= inits.decay
       state.rotInDrag[0] += state.vel[0]; state.rotInDrag[1] += state.vel[1]
       if (state.timer) state.timer = requestAnimationFrame(momentum)
     }
 
     function tick () {
-			
+
       if (state.autorotimer) state.autorotimer = requestAnimationFrame(tick)
-				
+
     }
 
     function stopMomentum () { cancelAnimationFrame(state.timer); state.timer = null }
@@ -38,7 +38,8 @@
       decay: 0.95,
       mult: 2e-3, // rotInDrag factor
       rotInit: [0, 0, 0],
-			timeSpan: 200,
+      timeSpan: 200,
+      epsilon: 1e-3,
     }
 
     // reset to default rotation
@@ -47,28 +48,28 @@
     }
 
     let state = {
-			
-			projection: d3.geoOrthographic(),	// _e_tbd 
-			
-			rotAccum: [0, 0, 0],
-			rotInDrag: [0, 0, 0], // rotInDrag in radians
-			
+
+      projection: null, // _e_tbd
+
+      rotAccum: [0, 0, 0],
+      rotInDrag: [0, 0, 0], // rotInDrag in radians
+
       grabbed: false,
       moved: false,
       rotVel: [0, 0, 0], // [-6e-3,7.6e-3,2.13e-3],   // [0,0,0],
-      
+
       vel: [0, 0, 0],
       moveSpan: 16,
-			autoRot: false,
+      autoRot: false,
       lastMoveTime: null,
       timer: null,
       autorotimer: null,
       rotMatrix: null,
       cPos: null,
       pPos: null
-			
-		}
-		
+
+    }
+
     // event position
     let getPos = e => (e.touches && e.touches.length) ? (e = e.touches[0], [e.x, e.y]) : [e.x, e.y]
 
@@ -85,28 +86,29 @@
       if (state.grabbed) return // drag ongoing
 
       stopMomentum()
-			
+
       state.moved = false // not moved yet
-			
+
       state.grabbed = getPos(e) // mouse position
-			state.pPos = state.grabbed // previous position 
+      state.pPos = state.grabbed // previous position
       state.cPos = state.pPos // current position
-			
+
       state.rotAccum = mgeom.add(state.rotAccum, state.rotInDrag) // rotation
-			
+
       rebase()
     }
 
     // dragged  listener
     let dragged = function () {
-      let e = d3.event
-
       if (!state.grabbed) return
+
+      let e = d3.event
       let pos = getPos(e) //  d3.mouse(this)
-			
+
+
       let dx = state.grabbed[1] - pos[1],
-					dy = pos[0] - state.grabbed[0]
-				
+          dy = - (state.grabbed[0] - pos[0])
+
       if (!state.moved) {
         if (dx * dx + dy * dy < state.moveSpan) return
         state.moved = true // moved
@@ -122,6 +124,7 @@
         state.rotVel[1] + dy * inits.mult,
         state.rotVel[2] + 0
       ]
+			
     }
 
     // dragended  listener
@@ -129,11 +132,13 @@
       if (!state.grabbed) return
       state.grabbed = false
       if (!state.moved) return
-      let f = Math.max(0, 1 - (Date.now() - state.lastMoveTime) / inits.timeSpan)
-      state.vel = [	// velocity
-					(state.pPos[1] - state.cPos[1]) * inits.mult * f, 
-					(state.cPos[0] - state.pPos[0]) * inits.mult * f	
-			]
+      let f = Math.max(0, 1 - (Date.now() - state.lastMoveTime))
+      state.vel = [ // velocity
+
+          (state.pPos[1] - state.cPos[1]) * inits.mult,
+          (state.cPos[0] - state.pPos[0]) * inits.mult 
+
+      ]
 
       state.timer = requestAnimationFrame(momentum)
     }
