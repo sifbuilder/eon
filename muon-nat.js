241d1824d57645e0e3c5931d2b36cea6388d8e6f
sifbuilder <sifbuilder@gmail.com>
2018-03-05 01:16:06 +0100

    421 fractals

--- muon-nat.js
+++ muon-nat.js
@@ -15,7 +15,7 @@
       mprofier = __mapper('xs').m('profier'),
       mproj3ct = __mapper('xs').m('proj3ct')
 
-    let cache = {} // points, form
+    let cache = {} // feature, form
 
     const cos = Math.cos, sin = Math.sin,
       neg = x => x < 0 || (x === 0 && (1 / x < 0)),
@@ -25,10 +25,10 @@
       tau = 2 * Math.PI
 
     /**********************
-   *    @nform
+   *    @natNform
    *      compleate form for natform
    */
-    let nform = function (form, nformed = {}) {
+    let natNform = function (form, nformed = {}) {
       let defs = {'v0': 0, 'v1': 1, 'ra2': 120, 'w4': 0, 'seg5': 360, 'pa6': 0, 'pb7': -1} // defs
 
       if (form && typeof form === 'object' && // {nat}
@@ -105,117 +105,6 @@
       return nformed
     }
 
-    /* **************************
-     *        @rador : seg5 unit circle rador
-     *          m.snap.snap (dax form => rador)
-     */
-    let rador = function (forml) { // polarCoords
-      let pts = []
-      let t = 0
-      let maxRadio = 0
-
-      if (f.isSame(forml, cache.forml)) {
-        
-        pts = cache.points
-        
-      } else {
-        
-        const {m1, m2, n1, n2, n3, a, b, v0, v1, seg5} = forml
-        const angUnit = tau / seg5 // dots per period
-
-        let angi = (forml.angi) ? forml.angi : (i, ang) => (i * ang) - Math.PI
-        let abs = (forml.abs) ? forml.abs : Math.abs
-
-        for (let i = 0; i < seg5; i++) {
-          let ang = angi(i, angUnit * v1) // [0,360] => [-180,180] // v1
-
-          let t1 = m1 * ang / 4
-          let t2 = m2 * ang / 4
-
-          t = Math.pow(
-
-            Math.pow(abs(Math.cos(t1) / a), n2) // n2
-                     +
-                     Math.pow(abs(Math.sin(t2) / b), n3), // n3
-
-            -1 / n1) // n1
-
-          t = t * (1 + v0 * i)
-
-          if (t > maxRadio) maxRadio = t
-          pts.push(t)
-        }
-
-        let radUnit = 1 / maxRadio //  Math.SQRT1_2 / maxRadio  normalize
-        pts = pts.map(d => d * radUnit)
-
-        cache.forml = forml
-        cache.points = pts
-      }
-
-      return pts // dots in path: [0,...,seg5] => [0,1]
-    }
-
-    /* **************************
-     *        @radorm
-     *            g.natform
-     */
-    function radorm (form, s1extent = [-1, 1]) { //  radorm: [-1,1) => [-1,1]
-      let radorPts = rador(form) //  rador:  [-1,1] => [0,seg5)
-      let s1range = [0, radorPts.length - 1] // [0, seg5]
-
-      let s2extent = d3.range(0, radorPts.length - 1) // [0,...,seg5]
-      let s2range = radorPts // mormed form
-
-      let s1 = d3.scaleLinear().domain(s1extent).range(s1range) // [-1,1] => [0,seg5]
-      let s2 = d3.scaleLinear().domain(s2extent).range(s2range) // [0,..,seg5] => rador
-
-      return p => s2(s1(p)) //  [0,1) =s1=> [0,seg5) =rador=> [0,1]
-    }
-
-    /* *********************
-   *    @natform
-   *      called by g.natform.pointStream to build nat conform point stream
-   *      callls m.nat.radorm
-   */
-    let natform = function (form) { // getVertex
-if (1 && 1) console.log("m.nat.natform form", form)      
-      let nformed = nform(form) // nform
-
-      
-      let unfeld = Object.values(nformed)
-
-      let dominos = unfeld.map(d => d.dom3) // [ [-180,180], [-180,180], [-90,90], [-90,90] ]
-
-      let radions = unfeld.map((d, i) => radorm(d, dominos[i])) // radorm
-      let rayscale = unfeld.map((d, i) => p => radions[i](p* degrees)) // rayscale on degres
-
-      let scale = [1, 1, 1], rotation = [0, 0, 0], location = [0, 0, 0], rad, wr, wd
-      if (nformed) rad = scale = unfeld.map(dax => dax.ra2)
-      if (nformed) wd = rotation = unfeld.map(dax => (dax.w4 || 0 )) //  yfase
-
-      let vertex = function (lambdaD, phiD = 0, radio = 1) { // spherical degrees
-      
-        let ppD = []      // pars in degrees
-          ppD[0] = lambdaD + wd[0]
-          ppD[1] = lambdaD + wd[1]
-          ppD[2] = phiD    + (wd[2] || 0)
-          ppD[3] = phiD    + (wd[3] || 0)
-          
-        let ppR = ppD.map( d => d * radians)  // pars in radians per dax
-        let rs = unfeld.map( (d,i) => rayscale[i](ppR[i]) || 1)  // radorn on dax par
-        let rr = unfeld.map( (d, i) => d.fn0(...ppR, ...rs) ) // 
-        
-        let point = unfeld.map( (d, i) => radio * rad[i] * rr[i])
-
-        let projpnt = (point[2] !== undefined) ? 
-              [ point[0], point[1], point[2] ] :    // 3D
-              [ point[0], point[1] ]                // 2D
-        return projpnt // [x,y,z]
-      }
-
-      return vertex
-    }
 
     /**********************
      *    @natFeature
@@ -225,69 +114,84 @@ if (1 && 1) console.log("m.nat.natform form", form)
       
       if (1 && 1) console.log("m.nat.natFeature form", form)
       
-      let nformed = nform(form) // NFORM
-      if (0 && 1) console.log('m.nat.natFeature:nformed', nformed)
-      let geometry
-      let dx, dy, sx, sy
+      let feature
+  
+      if (f.isSame(form, cache.form)) {
+        
+        feature = cache.feature
+        
+      } else {  
+    
+    
+            
+              let nformed = natNform(form) // NFORM
+              if (0 && 1) console.log('m.nat.natFeature:nformed', nformed)
+              let geometry
+              let dx, dy, sx, sy
 
-      if (Object.keys(nformed).length > 2) { // 3d
-        dx = 360 / nformed.x.seg5 // x
-        dy = 360 / nformed.z.seg5 // z
-        sx = dx
-        sy = dy
+              if (Object.keys(nformed).length > 2) { // 3d
+                dx = 360 / nformed.x.seg5 // x
+                dy = 360 / nformed.z.seg5 // z
+                sx = dx
+                sy = dy
 
-        
-        
-        let graticule = {frame: [ [ [-180, 180, sx, dx], [-90, 90, sy, dy] ] ]} // x, y
-        
-        
-        geometry = mgraticule.gedges(graticule).geometry
-        
-        
-      } else { // 2d
+                
+                
+                let graticule = {frame: [ [ [-180, 180, sx, dx], [-90, 90, sy, dy] ] ]} // x, y
+                
+                
+                geometry = mgraticule.gedges(graticule).geometry
+                
+                
+              } else { // 2d
+              
+                dx = 360 / nformed.x.seg5 // x
+                dy = 360 / nformed.y.seg5 // y
+                sx = 360
+                sy = 360
+                
+                // _e_
+                let graticule = {frame: [ [ [-180, 180, sx, dx], [-180, 180, sy, dy] ] ]} // x, y
+                geometry = mgraticule.gedges(graticule).geometry
+                let p = geometry.coordinates[1].slice(0, -1)
+                p = [...p, p[0]]
+
+                geometry.coordinates = Array.of(p)
+              }
+
+              if (0 && 1) console.log('m.nat.geometry', geometry)
+
+              let gj = {
+                type: 'Feature',
+                geometry: geometry,
+                properties: {
+                  docnat: '[[long,lat]] def radio:1',
+                  geonode: {
+                    type: 'Feature',
+                    geometry: {
+                      type: 'Point',
+                      coordinates: [0, 0, 0]
+                    },
+                    properties: {
+                      orgen: [0, 0, 0],
+                      velin: [0, 0, 0],
+                      velang: [0, 0, 0],
+                      prevous: [0, 0, 0],
+                      geodelta: [0, 0, 0]
+                    }
+                  }
+                }
+              }
+
+              let projection = mprofier.profion({'projection': 'natform', 'form': nformed})
+
+              feature = mproj3ct(gj, projection)
+
+         cache.form = form
+         cache.feature = feature  
+      }               
       
-        dx = 360 / nformed.x.seg5 // x
-        dy = 360 / nformed.y.seg5 // y
-        sx = 360
-        sy = 360
-        // _e_
-        let graticule = {frame: [ [ [-180, 180, sx, dx], [-180, 180, sy, dy] ] ]} // x, y
-        geometry = mgraticule.gedges(graticule).geometry
-        let p = geometry.coordinates[1].slice(0, -1)
-        p = [...p, p[0]]
-
-        geometry.coordinates = Array.of(p)
-      }
-
-      if (0 && 1) console.log('m.nat.geometry', geometry)
-
-      let feature = {
-        type: 'Feature',
-        geometry: geometry,
-        properties: {
-          docnat: '[[long,lat]] def radio:1',
-          geonode: {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [0, 0, 0]
-            },
-            properties: {
-              orgen: [0, 0, 0],
-              velin: [0, 0, 0],
-              velang: [0, 0, 0],
-              prevous: [0, 0, 0],
-              geodelta: [0, 0, 0]
-            }
-          }
-        }
-      }
-
-      let projection = mprofier.profion({'projection': 'natform', 'form': nformed})
-
-      let natgj = mproj3ct(feature, projection)
-
-      return natgj
+      return feature
     }
     /***************************
      *        @enty
@@ -295,8 +199,7 @@ if (1 && 1) console.log("m.nat.natform form", form)
     let enty = function () {}
 
     enty.natFeature = natFeature
-    enty.natform = natform
-    enty.nform = nform
+    enty.natNform = natNform
 
     return enty
   }
--- muon-snap.js
+++ muon-snap.js
@@ -14,124 +14,130 @@
       mlacer = __mapper('xs').m('lacer'),
       mgeoj = __mapper('xs').m('geoj')
 
+    let cache = {}
+      
     /***********
    *    @snap : anigram, t, flag
    */
     let snap = function snap (v, t = 0, g = 0) {
+      
+      
+            // ____________________________________________________ un-tagged
+            if (v === null) return null // 00 _____ o
+            else if (typeof (v) === 'number') return v // 02 _____ num
+            else if (typeof (v) === 'string') return v // 03 _____ str
+            else if (f.isArray(v) && v.length === 0) return v // 04 _____ []
+            else if (typeof (v) === 'function' &&
+              g !== 1) {
+              return v // 01 _____ fn v(t)
+            } else if (f.isArray(v) && // 05 ____ [[ [ pure ] ]]  intra array interpolation
+              f.isDoubleSingleArray(v) && // double array with single elem
+              f.isPureArray(v[0][0]) && // single elem in double array is pure
+              g !== 1
+            ) {
+              let ws = snap(v[0][0], t, 1)
+              return ws
+            } else if (f.isObject(v) && // 06 ___ v :: {}
+                g !== 1) {
+              let r = {}
+              for (let y of Reflect.ownKeys(v)) {
+                r[y] = snap(v[y], t) // reenter
+              }
+              return r
+            } else if (f.isDoubleArray(v) && // 07 [[ [ [], [] ] ]]   inter arrays interpolation
+              f.isQuasiPureArray(v[0][0]) && // double array with array of arrays elem
+              v[0][0].length === 1 &&
+              g !== 1
+            ) {
+              let na = []
+              for (let i = 0; i < v[0][0].length; i++) {
+                let comp = v[0][0][i]
 
-      // ____________________________________________________ un-tagged
-      if (v === null) return null // 00 _____ o
-      else if (typeof (v) === 'number') return v // 02 _____ num
-      else if (typeof (v) === 'string') return v // 03 _____ str
-      else if (f.isArray(v) && v.length === 0) return v // 04 _____ []
-      else if (typeof (v) === 'function' &&
-        g !== 1) {
-        return v // 01 _____ fn v(t)
-      } else if (f.isArray(v) && // 05 ____ [[ [ pure ] ]]  intra array interpolation
-        f.isDoubleSingleArray(v) && // double array with single elem
-        f.isPureArray(v[0][0]) && // single elem in double array is pure
-        g !== 1
-      ) {
-        let ws = snap(v[0][0], t, 1)
-        return ws
-      } else if (f.isObject(v) && // 06 ___ v :: {}
-          g !== 1) {
-        let r = {}
-        for (let y of Reflect.ownKeys(v)) {
-          r[y] = snap(v[y], t) // reenter
-        }
-        return r
-      } else if (f.isDoubleArray(v) && // 07 [[ [ [], [] ] ]]   inter arrays interpolation
-        f.isQuasiPureArray(v[0][0]) && // double array with array of arrays elem
-        v[0][0].length === 1 &&
-        g !== 1
-      ) {
-        let na = []
-        for (let i = 0; i < v[0][0].length; i++) {
-          let comp = v[0][0][i]
+                let intra = snap(comp, t, 1)
+                na.push(intra)
+              }
+              let ws = snap(na, t, 1) // scales of internal array
 
-          let intra = snap(comp, t, 1)
-          na.push(intra)
-        }
-        let ws = snap(na, t, 1) // scales of internal array
+              return ws
+            } else if (f.isArray(v) && // 08a ____ [[[ fn() ]]]
+              f.isTripleArray(v) &&
+              typeof v[0][0][0] === 'function' &&
+              g !== 1
+            ) {
+              let fn = v[0][0][0]
+              let ws = snap(fn, t, 1) // snap function value
+              return ws
+            } else if (f.isArray(v) && // 08 ____ [ [[ [ ], {} ]] ]
+              f.isTripleArray(v) &&
+              g !== 1
+            ) {
+              let ws = snap(v[0][0][0], t, 1) // scales of internal array
 
-        return ws
-      } else if (f.isArray(v) && // 08a ____ [[[ fn() ]]]
-        f.isTripleArray(v) &&
-        typeof v[0][0][0] === 'function' &&
-        g !== 1
-      ) {
-        let fn = v[0][0][0]
-        let ws = snap(fn, t, 1) // snap function value
-        return ws
-      } else if (f.isArray(v) && // 08 ____ [ [[ [ ], {} ]] ]
-        f.isTripleArray(v) &&
-        g !== 1
-      ) {
-        let ws = snap(v[0][0][0], t, 1) // scales of internal array
+              return ws
+            } else if (f.isArray(v) && // 09 ____ [[[ ], {}]] // last chance for the array
+              g !== 1
+            ) {
+              let ws = v.map(d => snap(d, t, 0))
+              return ws
+            }
 
-        return ws
-      } else if (f.isArray(v) && // 09 ____ [[[ ], {}]] // last chance for the array
-        g !== 1
-      ) {
-        let ws = v.map(d => snap(d, t, 0))
-        return ws
-      }
+            // ____________________________________________________ tagged
 
-      // ____________________________________________________ tagged
-
-      else if (typeof (v) === 'function' &&								// 01 _____ fn snappable time function
-                                      g === 1) {
-        return snap(v(t), t, 0)
-      } else if (f.isObject(v) && // 10 ___ v :: {b, c, d ...}*
-                                      g === 1) {					// assume nat on object
-        let ws
-        
-        let feature = mnat.natFeature(v)
-        if (!mgeoj.isValid(feature)) {
-            console.error("gj not valid", v, feature)
-        }
-        let geometry = feature.geometry
-        let natRing
-        if (geometry.type === "LineString") {
-          natRing = geometry.coordinates
-        } else if (geometry.type === "MultiLineString") {
-          natRing = geometry.coordinates[0]		// first line
-        } else if (geometry.type === "Polygon") {
-          natRing = geometry.coordinates[0]  // outer ring
-        } else if (geometry.type === "MultiPolygon") {
-          natRing = geometry.coordinates[0][0]		// outer ring of first polygon
-        } else {
-          console.error("g type not supported")
-        }
-        ws = snap(natRing, t, 1)				// (13) snap [[x1,y1,z1],...,[xn,yn,zn]]
-    
-        return ws
-      } else if (f.isArray(v) && // 11_____ [v]*
-          f.isPureArray(v) &&
-          v.length === 1 &&
-          g === 1) {
-        let d = [0, 1], r = [v[0], v[0]]
-        let w = d3.scaleLinear().domain(d).range(r)
-        return w(t)
-      } else if (f.isArray(v) && // 12 _____ [v1,v2,v3]*
-          f.isPureArray(v) &&
-          v.length > 1 &&
-          g === 1) {
-        let d = v.map((item, idx) => idx / (v.length - 1))
-        let r = v
-        let w = d3.scaleLinear()
-          .domain(d)
-          .range(r)
-        return w(t)
-      } else if (f.isArray(v) && // 13 _____ [[a1,a2,a3],[b1,b2]]*
-          f.isQuasiPureArray(v) && // => [[a1,b1],[a2,b1'],[a3,b2]]
-          g === 1) {													// [][] dosnap qualifier
-        let ws = mlacer.unslide(v).filter(d => d.length > 0).map(d => snap(d, t, 1))
-        return ws
-      } else {
-        return v
-      }
+            else if (typeof (v) === 'function' &&								// 01 _____ fn snappable time function
+                                            g === 1) {
+              return snap(v(t), t, 0)
+            } else if (f.isObject(v) && // 10 ___ v :: {b, c, d ...}*
+                                            g === 1) {					// assume nat on object
+              let ws
+              
+              let feature = mnat.natFeature(v)
+              if (!mgeoj.isValid(feature)) {
+                  console.error("gj not valid", v, feature)
+              }
+              let geometry = feature.geometry
+              let natRing
+              if (geometry.type === "LineString") {
+                natRing = geometry.coordinates
+              } else if (geometry.type === "MultiLineString") {
+                natRing = geometry.coordinates[0]		// first line
+              } else if (geometry.type === "Polygon") {
+                natRing = geometry.coordinates[0]  // outer ring
+              } else if (geometry.type === "MultiPolygon") {
+                natRing = geometry.coordinates[0][0]		// outer ring of first polygon
+              } else {
+                console.error("g type not supported")
+              }
+              ws = snap(natRing, t, 1)				// (13) snap [[x1,y1,z1],...,[xn,yn,zn]]
+          
+              return ws
+            } else if (f.isArray(v) && // 11_____ [v]*
+                f.isPureArray(v) &&
+                v.length === 1 &&
+                g === 1) {
+              let d = [0, 1], r = [v[0], v[0]]
+              let w = d3.scaleLinear().domain(d).range(r)
+              return w(t)
+            } else if (f.isArray(v) && // 12 _____ [v1,v2,v3]*
+                f.isPureArray(v) &&
+                v.length > 1 &&
+                g === 1) {
+              let d = v.map((item, idx) => idx / (v.length - 1))
+              let r = v
+              let w = d3.scaleLinear()
+                .domain(d)
+                .range(r)
+              return w(t)
+            } else if (f.isArray(v) && // 13 _____ [[a1,a2,a3],[b1,b2]]*
+                f.isQuasiPureArray(v) && // => [[a1,b1],[a2,b1'],[a3,b2]]
+                g === 1) {													// [][] dosnap qualifier
+              let ws = mlacer.unslide(v).filter(d => d.length > 0).map(d => snap(d, t, 1))
+              return ws
+            } else {
+              return v
+            }
+            
+      
+      
     }
 
     /***********
