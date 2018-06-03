8fccb0b94c34dfb8e1862c16d3d1d60466143b96
sifbuilder <sifbuilder@gmail.com>
2018-06-03 23:02:22 +0100

    s.enxl

--- script-enxl.js
+++ script-enxl.js
@@ -1,8 +1,17 @@
- 
+<!-- d3 -->
 <script src='https://d3js.org/d3.v5.min.js'></script>  
-<script src='https://d3js.org/d3-geo.v1.min.js'></script> 
-<script src='https://d3js.org/d3-geo-projection.v2.min.js'></script> 
 <script src='https://unpkg.com/d3-require@1.0.3/dist/d3-require.min.js'></script> 
+<script src="https://unpkg.com/d3-octree/build/d3-octree.min.js"></script>
+
+<script src="https://unpkg.com/versor"></script> <!-- for inertia -->
+<script src="https://unpkg.com/d3-inertia"></script>
+
+<script src='https://d3js.org/d3-geo.v1.min.js'></script> 
+<script src='https://unpkg.com/d3-geo-voronoi@0.0.6/build/d3-geo-voronoi.min.js'></script> 
+<script src='https://d3js.org/d3-geo-projection.v2.min.js'></script> 
+
+<!-- <script src='https://unpkg.com/d3-geo-polygon@1.4.2/build/d3-geo-projection-clip-polyhedral.js'></script>   -->
+<script src='https://unpkg.com/d3-geo-polygon@1.4.2/build/d3-geo-polygon.min.js'></script> 
 
 <!-- <script src='https:////unpkg.com/d3-force-3d@1.0/build/d3-force-3d.bundle.min.js'></script> -->
 <script src='https://unpkg.com/3d-force-graph@1.22.0/dist/3d-force-graph.min.js'></script> 
@@ -12,17 +21,17 @@
 <script src="https://unpkg.com/d3-force-bounce/dist/d3-force-bounce.min.js"></script>
 <script src="https://unpkg.com/d3-force-pod/dist/d3-force-pod.min.js"></script>
 
-
-<script src="https://unpkg.com/d3-octree/build/d3-octree.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.3.7/papaparse.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/qwest/4.5.0/qwest.min.js"></script> -->
-
+<!-- topojson.js -->
 <script src='https://cdnjs.cloudflare.com/ajax/libs/topojson/3.0.0/topojson.min.js'></script>
 
+<!-- three.js -->
 <script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/84/three.min.js'></script>
 <script src='https://unpkg.com/three-trackballcontrols-web@0.0.2/dist/three-trackballcontrols.min.js'></script>
 
 <script src='https://unpkg.com/mathjs@4.0.1/dist/math.min.js'></script>
 <script src='https://cdn.jsdelivr.net/npm/@turf/turf@5/turf.min.js'></script>
+
+<!-- tensorflowjs -->
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@0.8.0"></script>
+
 
