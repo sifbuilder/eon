130606243a924edd64102df368772fa78237af79
sifbuilder <sifbuilder@gmail.com>
2018-06-19 18:57:38 +0100

    852d x.mapper

--- x-mapper.js
+++ x-mapper.js
@@ -11,6 +11,28 @@
   let xMapper = function xMapper () {
     let state = {}
 
+    
+    async function report (part) {
+      let name = part[0] // name
+      let parts = Array.isArray(part[1]) ? part[1] : Array.of(part[1]) // to array
+
+      let promises = xD3Require.require(...parts) // destructure value
+      let [resolved] = await Promise.all([promises])
+      return [ name, resolved ]
+    }
+
+    async function reports (parts) {
+      let promises = parts.map(p => enty.report(p))
+      await Promise.all(promises)
+        .catch(function (err) {
+          console.log('A promise failed to resolve', err)
+          return promises
+        })
+        .then(function (resolveds) {
+          resolveds.map(r => enty({[r[0]]: r[1] }))
+        })
+    }
+
     let enty = function (_) {
       if (arguments.length < 1) return state
 
@@ -18,6 +40,9 @@
 
       if ((typeof _ === 'string') && (state[_] !== undefined)) return state[_]
     }
+
+    enty.report = report
+    enty.reports = reports
     return enty
   }
 
