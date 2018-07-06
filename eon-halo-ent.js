db14ed08c10dfef48cf3be4921bcc81f6cc16256
sifbuilder <sifbuilder@gmail.com>
2018-07-06 11:31:13 +0100

    h.ent wip

--- eon-halo-ent.js
+++ eon-halo-ent.js
@@ -28,14 +28,19 @@
 
     // ............................. conform
     const c = (f, xpromise) => {
-      return xpromise.then( x => {
-        let feature
-        feature = x(f) // CONFORM
-        feature.properties.formConformed = mgeoj.deprop(feature) // store conform
-        feature.properties.nodeConformed = feature.properties.geonode
-        if (1 && 1) console.log('conform feature', feature)
-        return feature
-      })
+      let feature = f
+      if (xpromise) { // CONFORM
+        return xpromise.then( x => {
+          if (1 && 1) console.log('_conform_', feature, x)
+
+          feature = x(feature) // CONFORM
+          feature.properties.formConformed = mgeoj.deprop(feature) // store conform
+          feature.properties.nodeConformed = feature.properties.geonode
+
+          return feature
+        })
+      }
+      return feature
     }
 
     // ............................. ereform
@@ -43,11 +48,13 @@
       let feature = f
       if (xpromise) { // EREFORM
         return xpromise.then(x => {
+                    if (1 && 1) console.log('_ereform_', feature, x)
           feature = mproj3ct(feature, x)
           feature.properties.formEreformed = mgeoj.deprop(feature) // store ereform
           feature.properties.nodeEreformed = mproj3ct(feature.properties.nodeConformed, x) //
           nodeConformed => nodeEreformed
-        return feature})
+          return feature
+        })
       } else {
         feature.properties.formEreformed = feature.properties.formConformed
         feature.properties.nodeEreformed = feature.properties.nodeConformed
@@ -60,6 +67,7 @@
       let feature = f
       if (xpromise) { // PROFORM
         return xpromise.then(x => {
+                   if (1 && 1) console.log('_proform_', feature, x)
           feature = mproj3ct(feature, x)
           feature.properties.formProformed = mgeoj.deprop(feature) // store proform
           feature.properties.nodeProformed = mproj3ct(feature.properties.nodeEreformed, x)
@@ -72,61 +80,44 @@
     }
 
 
-    let transforms = ani => f => c(f, mprofier.conformer_(ani))
-                            .then(f => e(f, mprofier.ereformion_(ani)))
-                            .then(f => p(f, mprofier.proformion_(ani)))
 
-    // ............................. gramm
-    async function gramm (anima, newAnigrams = []) {
-      let anigram = manitem(anima).anigram()
+    const getgj = ani => {
 
-      if (1 && 1) console.log(' ----------- 1 h.ent gramm', anigram)
-
-
-
-      // let conformed = f => c(f, mprofier.conformer_(anigram)),
-          // ereformed = f => e(f, mprofier.ereformion_(anigram)),
-          // proformed = f => p(f, mprofier.proformion_(anigram))
-
-
-      let gj
-      gj = mprops.v(anigram.geofold, anigram) // get geofold
+      let gj = mprops.v(ani.geofold, ani) // get geofold
       gj.properties = gj.properties || {} // recall genode
       gj.properties.geonode = gj.properties.geonode || {} // recall genode properties
       gj.properties.formGeoformed = mgeoj.deprop(gj) // store geoform
       gj.properties.nodeGeoformed = gj.properties.geonode // nodeGeoformed : geonode
-      if (1 && 1) console.log(' ----------- 2 h.ent gramm', anigram)
-
-      let gjcollection = mgeoj.featurecollect(gj) // FEATURE COLLECT
-      if (1 && 1) console.log(' ----------- 3 h.ent gramm', anigram)
-
-      let features = gjcollection.features
-      if (1 && 1) console.log(' ----------- 4 h.ent gramm', anigram)
-
-      // features = await Promise.all(features.map(feature => conformed(feature)))
-      // features = await Promise.all(features.map(feature => ereformed(feature)))
-      // features = await Promise.all(features.map(feature => proformed(feature)))
+      return gj
+    }
 
 
-      if (1 && 1) console.log(' ----------- 5 h.ent gramm', anigram)
+    let conformed = ani => f => c(f, mprofier.conformer_ (ani)),
+        ereformed = ani => f => e(f, mprofier.ereformion_(ani)),
+        proformed = ani => f => p(f, mprofier.proformion_(ani))
 
-      features = await Promise.all(features.map(f => transforms(anigram)(f)))
+    let transforms = ani => f => Promise.resolve(f)
+                            .then(f => c(f, mprofier.conformer_(ani)))
+                            .then(f => e(f, mprofier.ereformion_(ani)))
+                            .then(f => p(f, mprofier.proformion_(ani)))        
 
-      // features = await Promise.all(features.map(
-                                    // feature => conformed(feature)
-                                      // .then(feature => ereformed(feature) )
-                                      // .then(feature => proformed(feature) )
-                                      // ))
+    // ............................. gramm
+    function gramm (anima, newAnigrams = []) {
+
+      return Promise.resolve(anima)
+          .then(anima => manitem(anima).anigram())  // get anigram
+          .then(anigram =>  {
+              let gj = getgj(anigram)
+              let gjcollection = mgeoj.featurecollect(gj)
+              return Promise.all(gjcollection.features.map(f => transforms(anigram)(f)))
+                .then(newfeatures => {
+                    let newcollection = Object.assign({}, gjcollection, {features:newfeatures})
+                    let newanigram = Object.assign({}, anigram, {geofold: newcollection})
+                    return hformed.gramm(newanigram)
+                })
+          })
 
 
-
-if (1 && 1) console.log(' ------------ 6 features', features)
-
-  gjcollection.features = features
-
-
-      anigram.geofold = gjcollection
-      return hformed.gramm(anigram) // _
     }
 
     // ............................. enty
--- eon-render-svg.js
+++ eon-render-svg.js
@@ -116,6 +116,10 @@
           d => d.properties !== undefined && // req properties
             d.properties.ric !== undefined // req ric
         )
+        
+        if (1 && 1) console.log(' ------------------ render', features.length)
+
+        
       let svg = _svg()
 
       let cameraProjer = rrenderport.cameraProjer()
