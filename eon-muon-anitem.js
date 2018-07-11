3d1011bb6dd4616f65a71157c6df08e6eb1e4632
sifbuilder <sifbuilder@gmail.com>
2018-07-11 16:53:47 +0200

    m.anitem shorten

--- eon-muon-anitem.js
+++ eon-muon-anitem.js
@@ -21,80 +21,6 @@
 
     const functor = (d, ...p) => (typeof d === 'function') ? d(...p) : d
 
-    // ............................. nodeProformedSitus
-    let nodeProformedSitus = function (ani) {
-      let r = [0, 0, 0]
-      if (ani.geofold &&
-            ani.geofold.properties.geonode &&
-              ani.geofold.properties.geonode.properties.nodeProformed !== undefined) {
-        r = ani.geofold.properties.geonode.properties.nodeProformed.geometry.coordinates
-      }
-
-      return r
-    }
-
-    // ............................. nodeSitus
-    let nodeSitus = function (ani) {
-      if (2 && 2 && !ani)console.log(` * error: manitem.nodeSitus:ani: ${ani}`)
-      let r = [0, 0, 0]
-      if (ani.geofold &&
-            ani.geofold.properties.geonode) {
-        r = ani.geofold.properties.geonode.geometry.coordinates
-      }
-
-      return r
-    }
-
-    // ............................. parentCoords
-    let parentCoords = function (payload, coords = []) {
-      // if whole anitem has been passed, focus on payload
-      if (payload && payload.payload !== undefined) payload = payload.payload
-
-      let parentGeometry
-      let parentuid = payload.parentuid
-      let parent = __mapper('xs').m('store').findAnigramFromUid(parentuid) // mstack
-
-      if (parent !== undefined) {
-        // let geoj = parent.payload.ent
-        let geoj = parent.geofold
-        coords = mgeoj.getCoords(geoj)
-      }
-
-      return coords
-    }
-
-    // ............................. parentSitus
-    let parentSitus = function (payload, coords = []) {
-      // if whole anitem has been passed, focus on payload
-      if (payload && payload.payload !== undefined) payload = payload.payload
-
-      let parentGeometry
-      let parentuid = payload.parentuid
-      let parent = __mapper('xs').m('store').findAnigramFromUid(parentuid)
-
-      if (parent !== undefined) {
-        parent.payload.geonode = mgeonode.init(parent.payload.geonode)
-        coords = parent.payload.geonode.geometry.coordinates
-      }
-
-      return coords
-    }
-
-    // ............................. coreGeoform
-    let coreGeoform = () => p => ({ // geofold
-      type: 'Feature',
-      geometry: { type: 'Point', coordinates: [0, 0, 0]},
-      properties: {},
-    })
-
-    // ............................. coreGeonode
-    let coreGeonode = () => ({
-      type: 'Feature',
-      geometry: { type: 'Point', coordinates: null },
-      properties: {orgen: null, velin: null, velang: null, prevous: null, geodelta: null},
-    })
-
-
     // ............................. functorize
     let functorize = function (ani, t) {
       let anigram = ani
@@ -110,15 +36,12 @@
         anigram.geofold = functor((anigram.geofold), anigram)	// geofold
         anigram.payload.conform = functor(anigram.payload.conform, anigram)		// conform
         anigram.payload.proform = functor(anigram.payload.proform, anigram)		// proform
-
       }
       return anigram
     }
 
     let enty = () => {}
     enty.functorize = functorize
-
-
     return enty
   }
 
