b91541387b3f908d58656f7c3dff619d81413950
sifbuilder <sifbuilder@gmail.com>
2018-07-25 19:05:30 +0100

    619k

--- eon-x-version.js
+++ eon-x-version.js
@@ -1,12 +1,7 @@
 /*******************************************
 *      @xVersion
 */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports)
-    : typeof define === 'function' && define.amd ? define(['exports'], factory)
-      : (factory((global.xVersion = global.xVersion || {})))
-}(this, function (exports) {
-  'use strict'
+
 
   let xVersion = function () {
     let version = '0.0.1-rc1'
@@ -18,4 +13,3 @@
   }
 
   exports.xVersion = xVersion
-}))
