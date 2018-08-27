eb2e257ffca82a4d791b6fc97f564ebc94c9d7ea
sifbuilder <sifbuilder@gmail.com>
2018-08-27 19:37:56 +0100

    780 sim

--- eon-halo-formed.js
+++ eon-halo-formed.js
@@ -8,6 +8,21 @@
 }(this, function (exports) {
   'use strict'
 
+ // eon-halo-formed 
+// *process h.formed anigrams** 
+ 
+// xpects `a.geofold` to be `geojson.FeatureCollection` 
+// or the feature collection 
+ // order the collection on z dax, `mgeoj.zorder(gjcollection)` 
+ // identify to features on index, `mric.enric(ric, anigram, gjcollection)` 
+ 
+// hen for each feature, 
+ // boform, decoding style properties 
+ // set sort. `feature.properties.sort` will determine rendering. default to feature 
+ // inherit avatars 
+ 
+// then pass the collection back to `m.animation` for rendering  
+  
   async function haloFormed (__mapper = {}) {
     let [
       mric,
