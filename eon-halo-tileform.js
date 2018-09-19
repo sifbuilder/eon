a911db876dcf426e55a84424d262cd50ba0d7c49
sifbuilder <sifbuilder@gmail.com>
2018-09-19 23:34:30 +0100

    h.tileform

--- eon-halo-tileform.js
+++ eon-halo-tileform.js
@@ -1,14 +1,14 @@
 /****************************
- *      @haloTiles
+ *      @haloTileform
  */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports)
     : typeof define === 'function' && define.amd ? define(['exports'], factory)
-      : (factory((global.haloTiles = global.haloTiles || {})))
+      : (factory((global.haloTileform = global.haloTileform || {})))
 }(this, function (exports) {
   'use strict'
 
-  async function haloTiles (__mapper = {}) {
+  async function haloTileform (__mapper = {}) {
     let [
       mprops,
       muonGraticule,
@@ -24,11 +24,14 @@
 
     ])    
 
-
-    function getTiles (anigram, newItems = []) {
-      let geofold = anigram.geofold, // geofold
+    // .................... getTiles    
+    function getTiles (anigram) {
+      
+      let newItems = []
+      
+      let ric = anigram.ric, // ric
+        geofold = anigram.geofold, // geofold
         geonode = anigram.geonode, // geonode
-        ric = anigram.ric, // ric
         payload = anigram.payload // payload
 
       let gj = mprops.v(geofold, anigram) // get geofold
@@ -43,20 +46,12 @@
       let faces = muonGraticule.gfaces(graticule) // faces
 
       for (let i = 0; i < faces.length; i++) { // single ring polygon
-        let idx = i
-        let gid = ric.gid // from ava ric
-        let cid = ric.cid
-        let fid = ric.cid + '_' + idx
-        let _ric = {gid, cid, fid}
+        let face = faces[i]
 
-        let newItem = Object.assign({}, anigram)
+        let newItem = mprops.clone(anigram)
         newItem.halo = 'natform'
-        newItem.payload = Object.assign({}, anigram.payload)
-        newItem.ric = _ric
-
         newItem.geofold = {type: 'Polygon', coordinates: []}
 
-        let face = faces[i]
         let facering = [vertices[face[0]], vertices[face[1]], vertices[face[2]]]
         newItem.geofold.coordinates = Array.of([...facering, facering[0]])
 
@@ -66,23 +61,29 @@
       return newItems
     }
 
+    // .................... ween
+    function ween (anima) {
+      return (anima.inited !== 1) ? (anima.inited = anima.gelded = 1, [anima]) : []
+    }
+
     // .................... gramm
     function gramm (anigram) {
       return Promise.all(getTiles(anigram).map(ani => haloEoform.gramm(ani)))
     }
 
     // .................... enty
-    let haloNat_ween = anima => (anima.inited !== 1) ? (anima.inited = anima.gelded = 1, [anima]) : []
+    let haloNat_ween = anima => ween(anima)
     let haloNat_gramm = anima => gramm(anima)
 
-    let haloTiles = {}
-    haloTiles.ween = anima => haloNat_ween(anima)
-    haloTiles.gramm = anima => haloNat_gramm(anima)
+    let haloTileform = {}
+    haloTileform.ween = anima => haloNat_ween(anima)
+    haloTileform.gramm = anima => haloNat_gramm(anima)
 
-    let enty = haloTiles
+    let enty = haloTileform
 
     return enty
   }
 
-  exports.haloTiles = haloTiles
+  exports.haloTileform = haloTileform
 }))
+
