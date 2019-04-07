0a1117e51538da20a2c0905f8e52ebd226bc2b01
sifbuilder <sifbuilder@gmail.com>
2019-04-07 15:45:17 +0200

    z.419t

--- eon-z-419t-pacer-trace.js
+++ eon-z-419t-pacer-trace.js
@@ -71,6 +71,27 @@
           geometry: { type: 'Point', coordinates: [[[[0, 200]]], 0, 0] },
           properties: { pointRadius: 6 },
         },
+        eonode: {
+          type: 'Feature',
+          geometry: { type: 'Point', coordinates: [0, 0, 0] },
+          properties: {
+            orgen: [0, 0],
+            velin: [0, 0],
+            velang: [0, 0],
+            prevous: [0, 0],
+            geodelta: [0, 0],
+          },
+        },
+        eomot: {
+          proform: {
+            projection: 'uniwen',
+            translate: [0, 0, 0],
+            scale: 1,
+            rotate: [0, 0, 0],
+            lens: [0, 1, Infinity],
+            geoanod: 1,
+          },
+        },
         eocrom: {
           csx: 0,
           cf: 888,
@@ -79,8 +100,51 @@
           cw: 0.9,
           cp: 0.9,
         },
-        __avatars: {
-          avatar: {
+        avatars: {
+          line: {
+            eohal: eohalMars, // eohalPacer,
+            eoric: { gid: 'ava', cid: 'ava', fid: 'line' },
+            eotim: eotim,
+            eofold: ani => {
+              let parentani = muonStore.findAnigramFromUid(ani.eoric.pid)
+              let point = parentani.eofold.features[0].geometry.coordinates
+
+              let coordinates = [point]
+
+              let preani = muonStore.findAnigramFromUid(ani.eoric.uid)
+              if (preani !== undefined) {
+                if (preani.eofold.type === 'FeatureCollection') {
+                  let feature = preani.eofold.features[0]
+                  coordinates = feature.geometry.coordinates
+                } else if (preani.eofold.type === 'Feature') {
+                  let feature = preani.eofold
+                  coordinates = feature.geometry.coordinates
+                }
+              }
+
+              coordinates = [...coordinates, point]
+
+              let feature = {
+                type: 'Feature',
+                geometry: {
+                  type: 'LineString',
+                  coordinates: coordinates,
+                },
+              }
+
+              return feature
+            },
+            eocrom: {
+              csx: 0,
+              cf: 888,
+              co: 0.09,
+              cs: 555,
+              cw: 0.9,
+              cp: 0.9,
+            },
+          },
+
+          point: {
             eohal: eohalMars, // eohalPacer,
             eoric: { gid: 'ava', cid: 'ava', fid: 'dot' },
             eotim: eotim,
@@ -99,176 +163,7 @@
             },
           },
         },
-        eoload: {
-          pacer: {
-            pacedby: {
-              initN: 0,
-              eventN: 0,
-              autoN: 1,
-              autoP: 0.036,
-              autoT: 0.1,
-              outtimed: 0,
-              maxN: 60,
-              geospan: 0,
-              basePaceOnAniView: '', // undefined, base on resulting geofold
-              addItemToPacer: 1, // geometries will not be accumulated
-            },
 
-            anigram: {
-              eotim: eotim,
-              eohal: eohalMars,
-
-              eoric: function (ani, props) {
-                let eoric = { gid: 'g', cid: 'c', fid: 'paced' }
-                console.assert(ani.eoric.uid !== undefined)
-                eoric.pid = ani.eoric.uid
-                return eoric
-              },
-
-              eofold: function (ani, props) {
-                let coords = [0, 0]
-                let eoric = { gid: 'g', cid: 'c', fid: 'paced' }
-console.log('ani', ani)
-                // let preani = muonStore.findAnima(eoric)
-                let preani = muonStore.findAnigram(eoric) // _e_
-
-                let point = [
-                  200 * (0.5 - Math.random()),
-                  200 * (0.5 - Math.random()),
-                ]
-
-                if (preani === undefined) {
-                  coords = Array.of(coords)
-                } else {
-                  let precoords
-                  if (preani.eofold.geometry) {
-                    precoords = preani.eofold.geometry.coordinates
-                  }
-
-                  if (preani.eofold.type === 'FeatureCollection') {
-                    precoords = preani.eofold.features[0].geometry.coordinates // _e_
-                  }
-
-                  coords = [...precoords, point]
-                }
-
-                let geometry = {
-                  type: 'LineString',
-                  coordinates: coords,
-                }
-                console.assert(
-                  muonGeoj.isValid(geometry),
-                  `gj coords not valid in ${geometry}`
-                )
-
-                let feature = {
-                  type: 'Feature',
-                  geometry: geometry,
-                  properties: {},
-                }
-
-                return feature
-              },
-
-              eonode: function (ani, props) {
-                // eoric repeated in pacedItem eonode from pacedItem eoric
-                let eoric = { gid: 'g', cid: 'c', fid: 'paced' }
-                let stace = [0, 0, 0]
-
-                let coordinates = stace
-                let res = {
-                  type: 'Feature',
-                  geometry: { type: 'Point', coordinates: coordinates },
-                  properties: {
-                    orgen: null,
-                    velin: [0, 0, 0],
-                    velang: [0, 0, 0],
-                    prevous: null,
-                    geodelta: null,
-                  },
-                }
-                return res
-              },
-
-              eomot: {
-                proform: {
-                  projection: 'uniwen',
-                  translate: [0, 0, 0], // mot
-                  scale: 1,
-                  rotate: [[[[0, 360]]], [[[0, 360]]], [[[0, 360]]]],
-                  prerotate: [[[ctl.rotation]]],
-                  lens: [0, 1, Infinity],
-                  addNodeToTranslate: 1, // eonode
-                },
-              },
-
-              eoload: {
-                eocrom: {
-                  csx: 0,
-                  cf: 777,
-                  co: 0.1,
-                  cs: [[[999, 111, 999]]],
-                  cw: 0.5,
-                  cp: 1,
-                },
-
-                eoform: {
-                  x: {
-                    m1: 4,
-                    m2: 4,
-                    n1: 100,
-                    n2: 100,
-                    n3: 100,
-                    a: 1,
-                    b: 1, // square
-                    ra2: 20 * Math.sqrt(2),
-                    v0: 0,
-                    v1: 1,
-                    w4: 0,
-                    seg5: 8,
-                    pa6: 0,
-                    pb7: -1,
-                    dom3: [-180, 180],
-                  },
-                  y: {
-                    m1: 4,
-                    m2: 4,
-                    n1: 100,
-                    n2: 100,
-                    n3: 100,
-                    a: 1,
-                    b: 1, // square
-                    ra2: 20 * Math.sqrt(2),
-                    v0: 0,
-                    v1: 1,
-                    w4: 0,
-                    seg5: 8,
-                    pa6: 0,
-                    pb7: -1,
-                    dom3: [-180, 180],
-                  },
-                  z: {
-                    m1: 4,
-                    m2: 4,
-                    n1: 100,
-                    n2: 100,
-                    n3: 100,
-                    a: 1,
-                    b: 1, // square
-                    ra2: 20,
-                    v0: 0,
-                    v1: 1,
-                    w4: 0,
-                    seg5: 8,
-                    pa6: 0,
-                    pb7: -1,
-                    dom3: [-180, 180],
-                  },
-                },
-              },
-            },
-          },
-        },
       }
 
       // .................. scene
