c013581cd60b9f966e2738dff08f611fcc7822ee
sifbuilder <sifbuilder@gmail.com>
2019-04-19 00:40:47 +0200

    t.unshort

--- tee/eon-teer-unshort.js
+++ tee/eon-teer-unshort.js
@@ -13,6 +13,9 @@ const camelize = str => str
   .replace(/\s+/g, '') // remove white space
   .replace(/-+/g, '') // remove hyphen
 
+const snakefy = str => str  
+  .replace(/([a-z][A-Z])/g, function (g) { return g[0] + '-' + g[1].toLowerCase() })
+
 function escapeRegExp (string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
 }
@@ -29,7 +32,7 @@ let args = process.argv
 let [cmd, scp, ...opts] = args
 
 let action
-let inScopePattern = new RegExp('^(eon-z.*).(js)', 'i')
+let inScopePattern = new RegExp('^(eon-)(.*)(.js)', 'i')
 
 if (opts.length === 0) { // action: help
   action = 'help'
@@ -48,7 +51,7 @@ if (opts.length === 0) { // action: help
   } else {
     argpattern = opts[0]
   }
-  inScopePattern = new RegExp(`^(eon-z${argpattern}.*).(js)`, 'i')
+  inScopePattern = new RegExp(`^(eon-)(${argpattern}.*)(.js)`, 'i')
 }
 
 const appdir = '.'
@@ -56,20 +59,17 @@ const appdir = '.'
 let indexfiles = fs.readdirSync(appdir) // eon-z-.*.html files
   .filter(d => inScopePattern.test(d))
 
-
+console.log('indexfiles:', indexfiles)
 
 const regexFileNameParts = new RegExp('^(eon-z)(.*).(js)', 'i')
 
 const newHtmlText = `-`
 
-
-
 // .................. jsToJs
-function jsToJs (data) {
-  let {eonName, preEonName, text} = data
+function rep (data) {
+  let {frompattern, topattern, text} = data
 
-  let topattern = eonName
-  let searchexp = RegExp(`${preEonName}`, 'g')
+  let searchexp = RegExp(escapeRegExp(`${frompattern}`), 'g')
 
   let arr
   while ((arr = searchexp.exec(text)) !== null) {
@@ -86,116 +86,66 @@ function doit (data) {
   for (let i = 0; i < indexfiles.length; i++) {
     let fileName = indexfiles[i]
     let fileText = fs.readFileSync(fileName, 'utf8')
-    let fileTextParts = fileText.match(regexFileNameParts) // eg. [ 'eon-z-021a.html', 'eon-z', '-', '021a', 'html' ]
 
-    
+    // const regexEonParts = []
+    let regexEonParts = new RegExp('renderSvg', 'i')
+
+    // let fileTextParts = fileText.match(regexEonParts) // eg. [ 'eon-z-021a.html', 'eon-z', '-', '021a', 'html' ]
+
+    fileText = rep({frompattern: 'renderSvg', topattern: 'eonRenderSvg', text: fileText})
+    fileText = rep({frompattern: "r('svg')", topattern: "o('eonRenderSvg')", text: fileText})
+    console.log('fileText:', fileText)
+    return
 
     const eonName = camelize(newName)
     const preEonName = camelize(`z${corePart}`)
 
     if (existsFile(preFileJs)) { // .html + .js
-      if (action === 'show' || action === 'debug') {
-        console.log(` *********** HTML + JS`)
-        console.log(`${preFileHtml}`) // eon-z-815e-d2bernoulli.html
-        console.log(`  +  ${preFileJs}`) // eon-z-815e-d2bernoulli.js
-        console.log(` ---- will create ${newNameHtml}`)
-      }
 
-      if (action === 'debug') {
-        console.log(` ---- text of ${newNameHtml}`)
-        console.log(`${newHtmlText}`)
-      }
+      // if (action === 'show' || action === 'debug') {
+      //   console.log(` *********** HTML`)
+      //   console.log(`${preFileHtml}`) // eon-z-815e-d2bernoulli.html
+      //   console.log(` ---- will create ${newNameHtml}`)
+      // }
+      // if (action === 'debug') {
+      //   console.log(` ---- new text of ${newNameHtml}:`)
+      //   console.log(newHtmlText)
+      // }
 
-      if (action === 'doit') {
-        fs.writeFile(`${newNameHtml}`, `${newHtmlText}`, function (err) { // eon-z815e-d2bernoulli.html
-          if (err) throw err
-          console.log(` ----Updated ${newNameHtml}`)
-        })
+      // if (action === 'doit') {
+      //   fs.writeFile(`${newNameHtml}`, `${newHtmlText}`, function (err) { // eon-z815e-d2bernoulli.html
+      //     if (err) throw err
+      //     console.log(` ---- Updated ${newNameHtml}`)
+      //   })
 
-        fs.writeFile(`${newNameJs}`, '', function (err) { // eon-z815e-d2bernoulli.js
-          if (err) throw err
-          console.log(` ----Updated ${newNameJs}`)
-        })
-      }
+      // }
 
-      let fileJsText = fs.readFileSync(preFileJs, 'utf8')
-      fileJsText = jsToJs({eonName, preEonName, text: fileJsText})
+      // let fileHtmlText = fs.readFileSync(preFileHtml, 'utf8')
+      // let fileJsText = htmlToJs({eonName, text: fileHtmlText})
 
-      if (action === 'show' || action === 'debug') {
-        console.log(` ---- will create ${newNameJs}`)
-        console.log(` ----   from ${preFileJs}`)
-        console.log(` ---- replace pre-eno: ${preEonName}`) // z419ePacerNatEoloadAnify
-        console.log(` ----   by new eon name: ${eonName}`) // eonZ419ePacerNatEoloadAnify
-      }
-      if (action === 'debug') {
-        console.log(` ---- text of ${newNameJs}`)
-        console.log(`${fileJsText}`)
-      }
+      // if (action === 'show' || action === 'debug') {
+      //   console.log(` ---- will create ${newNameJs}`)
+      // }
+      // if (action === 'debug') {
+      //   console.log(` ---- new text of ${newNameJs}:`)
+      //   console.log(fileJsText)
+      // }
 
-      if (action === 'show' || action === 'debug') {
-        console.log(` ---- will delete ${preFileJs}`)
-        console.log(` ---- will delete ${preFileHtml}`)
-      }      
+      // if (action === 'show' || action === 'debug') {
+      //   console.log(` ---- will delete ${preFileHtml}`)
+      // }
 
-      if (action === 'doit') {
-        fs.writeFile(`${newNameJs}`, `${fileJsText}`, function (err) { // eon-z815e-d2bernoulli.js
-          if (err) throw err
-          console.log(` ---- Updated ${newNameJs}`)
-        })
-        fs.unlinkSync(`${preFileJs}`, function (err) { // eon-z-815e-d2bernoulli.js
-          if (err) throw err
-          console.log(` ---- Deleted ${preFileJs}`)
-        })
-        fs.unlinkSync(`${preFileHtml}`, function (err) { // eon-z-815e-d2bernoulli.html
-          if (err) throw err
-          console.log(` ---- Deleted ${preFileHtml}`)
-        })
-      }
-    } else { // .html
-      if (action === 'show' || action === 'debug') {
-        console.log(` *********** HTML`)
-        console.log(`${preFileHtml}`) // eon-z-815e-d2bernoulli.html
-        console.log(` ---- will create ${newNameHtml}`)
-      }
-      if (action === 'debug') {      
-        console.log(` ---- new text of ${newNameHtml}:`)
-        console.log(newHtmlText)
-      }
+      // if (action === 'doit') {
+      //   fs.writeFile(`${newNameJs}`, `${fileJsText}`, function (err) { // eon-z815e-d2bernoulli.js
+      //     if (err) throw err
+      //     console.log(` ---- Updated ${newNameJs}`)
+      //   })
 
-      if (action === 'doit') {
-        fs.writeFile(`${newNameHtml}`, `${newHtmlText}`, function (err) { // eon-z815e-d2bernoulli.html
-          if (err) throw err
-          console.log(` ---- Updated ${newNameHtml}`)
-        })
-
-      }
-
-      let fileHtmlText = fs.readFileSync(preFileHtml, 'utf8')
-      let fileJsText = htmlToJs({eonName, text: fileHtmlText})
-
-      if (action === 'show' || action === 'debug') {
-        console.log(` ---- will create ${newNameJs}`)
-      }
-      if (action === 'debug') {      
-        console.log(` ---- new text of ${newNameJs}:`)
-        console.log(fileJsText)
-      }
-
-      if (action === 'show' || action === 'debug') {
-        console.log(` ---- will delete ${preFileHtml}`)
-      }
-
-      if (action === 'doit') {
-        fs.writeFile(`${newNameJs}`, `${fileJsText}`, function (err) { // eon-z815e-d2bernoulli.js
-          if (err) throw err
-          console.log(` ---- Updated ${newNameJs}`)
-        })
-
-        fs.unlinkSync(`${preFileHtml}`, function (err) { // eon-z-815e-d2bernoulli.html
-          if (err) throw err
-          console.log(` ---- Deleted ${preFileHtml}`)
-        })
-      }
+      //   fs.unlinkSync(`${preFileHtml}`, function (err) { // eon-z-815e-d2bernoulli.html
+      //     if (err) throw err
+      //     console.log(` ---- Deleted ${preFileHtml}`)
+      //   })
+      // }
     }
   }
 }
